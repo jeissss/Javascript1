eb4e76727bcc842de48a1d94ebc42f3ee4b6887f
jeissss <jeisongomez@gmail.com>
2023-08-03 10:04:39 -0500

    Version2

--- script4.js
+++ script4.js
@@ -1,3 +1,13 @@
-/*let tiempo = prompt("Ingrese el tiempo que estuvo en el estacionamiento")
-let hora = 2000
-let minutos*/
+/*let valueByHour = 2000
+let quantityOfHour = prompt("Ingrese la cantidad de horas que estuvo en el estacionamiento")
+let quantityOfMinutes = prompt("Ingrese la cantidad de minutos")
+
+let totalPrice =0
+
+if (quantityOfMinutes > 0){
+    quantityOfHour = parseInt(quantityOfHour) + 1
+}
+
+totalPrice = quantityOfHour * valueByHour 
+
+alert("El precio que debe pagar es: " + totalPrice)*/
---
+++ script9.js
@@ -0,0 +1,8 @@
+/*let dias = prompt("Ingrese el numero de dias que va a viajar")
+let hotel = 40000
+let comida = 40000
+let otros= 200
+let suma = (Number(hotel) * Number(dias) + Number(comida) * Number(dias) + Number(otros) * Number(dias)) 
+
+alert("El monto de su cheque es de " + suma)*/
+
